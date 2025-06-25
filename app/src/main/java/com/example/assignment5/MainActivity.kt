badfa8ed867e9f5b0bd62e076a32cd274f0fbf20
w0855062 <Bhupendrac258@gmail.com>
2025-06-25 17:40:59 -0400

    Completed the assignment but checking on app

--- app/src/main/java/com/example/assignment5/MainActivity.kt
+++ app/src/main/java/com/example/assignment5/MainActivity.kt
@@ -1,47 +1,39 @@
 package com.example.assignment5
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.assignment5.ui.theme.Assignment5Theme
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.ceil
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() {
+    private lateinit var nameInput: EditText
+    private lateinit var greetingText: TextView
+    private lateinit var showButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Assignment5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        nameInput = findViewById(R.id.name_edit_text)
+        greetingText = findViewById(R.id.greeting_text_view)
+        showButton = findViewById(R.id.show_name_button)
+
+        showButton.setOnClickListener{
+            val name = nameInput.text.toString().trim()
+            if (name.isNotEmpty()){
+                greetingText.text = "Hello, $name!"
+            }else{
+                greetingText.text = "Please Enter the Name"
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Assignment5Theme {
-        Greeting("Android")
-    }
+
 }
